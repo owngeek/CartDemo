e40cd62ff31572eddbfc981edf8aeaf026ea7f4d
samseernbr <samseernbr@gmail.com>
2020-03-17 18:34:43 +0530

    initial commit

---
+++ src/component/CheckoutComponent.js
@@ -0,0 +1,15 @@
+import React from "react"
+class CheckoutComponent extends React.Component{
+    render(){
+        return(
+            <div className="wrpr_totalbx">
+            <div className="quality">
+            <h3>Qty 0</h3>
+            <h3>Total 0</h3>
+        </div>
+            <button>CHECKOUT</button>
+        </div>
+        )
+    }
+}
+export default CheckoutComponent
